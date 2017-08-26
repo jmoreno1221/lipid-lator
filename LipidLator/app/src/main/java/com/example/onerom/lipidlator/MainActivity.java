0ffeb43474bc3ce8958f1d04be851a7eb740a777
Jose Moreno <jmoreno5@ggc.edu>
2017-08-26 15:59:59 -0400

    Updating Project

--- LipidLator/app/src/main/java/com/example/onerom/lipidlator/MainActivity.java
+++ LipidLator/app/src/main/java/com/example/onerom/lipidlator/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.onerom.lipidlator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submit = (Button) findViewById(R.id.btnSubmit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "The Button ws pressed", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
