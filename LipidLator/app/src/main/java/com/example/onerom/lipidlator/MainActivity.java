34a2027c8f86a29c7a6baeb5fb12c4e727e41969
jmoreno1221 <jmoreno5@ggc.edu>
2017-08-28 01:33:33 -0400

    Updating project, added some functionality to the selection of IONS

--- LipidLator/app/src/main/java/com/example/onerom/lipidlator/MainActivity.java
+++ LipidLator/app/src/main/java/com/example/onerom/lipidlator/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import static com.example.onerom.lipidlator.R.id.parent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +19,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button submit = (Button) findViewById(R.id.btnSubmit);
+        final Spinner spnIon = (Spinner) findViewById(R.id.spnIon);
+
+        spnIon.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //String strChosenRace = (String) adapterView.getItemAtPosition(i);
+                String[] realName = getResources().getStringArray(R.array.ion_array_values);
+                Toast.makeText(MainActivity.this, "Item selected" + realName[i], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "The Button ws pressed", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
                 startActivity(intent);
 
             }
-        });
-    }
+
+        });}
+
+
+
 }
+
--- LipidLator/app/src/main/java/com/example/onerom/lipidlator/ResultsActivity.java
+++ LipidLator/app/src/main/java/com/example/onerom/lipidlator/ResultsActivity.java
@@ -1,7 +1,10 @@
 package com.example.onerom.lipidlator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Button back = (Button) findViewById(R.id.btnGoBack);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResultsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
