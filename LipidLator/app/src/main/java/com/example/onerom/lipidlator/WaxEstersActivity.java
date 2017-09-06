a98bfb1dae1849c74c0413eba08c8f88255de1af
Jessica Sok <jessicasok11@gmail.com>
2017-09-06 13:58:18 -0400

    Added Wax Esters Selection Screen

---
+++ LipidLator/app/src/main/java/com/example/onerom/lipidlator/WaxEstersActivity.java
@@ -0,0 +1,13 @@
+package com.example.onerom.lipidlator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaxEstersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wax_esters);
+    }
+}
