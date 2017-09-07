5852c6517d49b956f8f57bda8db7ab6eaad9ccc3
Jessica Sok <jessicasok11@gmail.com>
2017-09-07 12:26:03 -0400

    Worked on Screen placement for fatty acids, wax esters, acyl carnitines, coa esters, and glycerolipids activity screens. Commented some codes in manifest.

---
+++ LipidLator/app/src/main/java/com/example/onerom/lipidlator/CoA_EsterActivity.java
@@ -0,0 +1,13 @@
+package com.example.onerom.lipidlator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoA_EsterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_co_a__ester);
+    }
+}
