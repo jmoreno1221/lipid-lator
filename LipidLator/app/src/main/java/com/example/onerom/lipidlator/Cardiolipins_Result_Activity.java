1826ebc92d55c76921f5d9e24c6d6881f507c045
Joseph Tsegaye <josephtsegaye@gmail.com>
2017-09-17 23:45:50 -0400

    Back Button on Selection Screen and Calculation Screen

---
+++ LipidLator/app/src/main/java/com/example/onerom/lipidlator/Cardiolipins_Result_Activity.java
@@ -0,0 +1,13 @@
+package com.example.onerom.lipidlator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cardiolipins_Result_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardiolipins__result_);
+    }
+}
---
+++ LipidLator/app/src/main/java/com/example/onerom/lipidlator/Sphingolipids_Result_Activity.java
@@ -0,0 +1,13 @@
+package com.example.onerom.lipidlator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sphingolipids_Result_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sphingolipids__result_);
+    }
+}
