3e3dc5f654857fcad56dda8e54a2f209d8a6360b
Jose Moreno <jmoreno5@ggc.edu>
2017-09-21 19:17:09 -0400

    fresh start

---
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/Loading.java
@@ -0,0 +1,38 @@
+package teamtriplej.com.lipidlator21;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class Loading extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        tv = (TextView) findViewById(R.id.appTitle);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(animation);
+        final Intent i = new Intent(this, HomeActivity.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally{
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
