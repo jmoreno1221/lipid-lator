8cea9c3c6c7c8544e01d7212b99903347d555e8a
jmoreno1221 <jmoreno5@ggc.edu>
2017-10-09 23:44:57 -0400

    Added new icon image and have added a logo

--- Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/Loading.java
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/Loading.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Loading extends AppCompatActivity {
 
     private TextView tv;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +19,15 @@ public class Loading extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
 
         tv = (TextView) findViewById(R.id.appTitle);
+        logo = (ImageView) findViewById(R.id.logo);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        logo.startAnimation(animation);
         tv.startAnimation(animation);
         final Intent i = new Intent(this, HomeActivity.class);
         Thread timer = new Thread(){
             public void run(){
                 try {
-                    sleep(1750);
+                    sleep(2500);
                 } catch (InterruptedException e){
                     e.printStackTrace();
                 }
