8b781088f735b3bfdf8b45b0efc99b87d8495dff
Jessica Sok <jessicasok11@gmail.com>
2017-10-12 17:51:35 -0400

    Worked on getting the CoA Result to display

--- Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/CoA_Esters_Result_Activity.java
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/CoA_Esters_Result_Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CoA_Esters_Result_Activity extends AppCompatActivity {
 
@@ -14,6 +15,31 @@ public class CoA_Esters_Result_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_co_a_esters_result);
         Button btnBack = (Button) findViewById(R.id.btnBack);
         Button btnHome = (Button) findViewById(R.id.btnHome);
+
+        TextView ionResult = (TextView) findViewById(R.id.tvIonResult);
+        TextView acylResult = (TextView) findViewById(R.id.tvAcylChainResult);
+        TextView abvResult = (TextView) findViewById(R.id.tvAbbreviationResult);
+        TextView formulaResult = (TextView) findViewById(R.id.tvFormulaResult);
+        TextView molarMassResult = (TextView) findViewById(R.id.tvMolarMassResult);
+
+        int ionIndex = getIntent().getExtras().getInt("ionIndex");
+        int acylIndex = getIntent().getExtras().getInt("acylIndex");
+        String ionSelected = getIntent().getExtras().getString("ionSelected");
+        String acylSelected = getIntent().getExtras().getString("acylSelected");
+
+        Calculations calc = new Calculations();
+        double mass = calc.calculateCoABasicMass(acylIndex);
+        double molarMass = Math.round(calc.calculateFinalMass(ionIndex, mass) * 10000d)/ 10000d;
+        String formula = calc.calculateFormula(calc.getNumC(), calc.getNumH(), calc.getNumO(), calc.getNumN(),
+                calc.getNumAg(), calc.getNumLi(), calc.getNumNa(), calc.getNumK(), calc.getNumCl(),
+                calc.getNumP(), calc.getNumS(), calc.getNumF());
+
+        molarMassResult.setText(Double.toString(molarMass));
+        formulaResult.setText(formula);
+        ionResult.setText(ionSelected);
+        acylResult.setText(acylSelected);
+        abvResult.setText("CoA(" + acylSelected + ")");
+
         btnBack.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
