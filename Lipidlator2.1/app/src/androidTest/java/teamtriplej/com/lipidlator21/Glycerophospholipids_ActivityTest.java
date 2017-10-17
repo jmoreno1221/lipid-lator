2ce71d857d3c789184414b80dcaf895038a15f54
Jose Moreno <jmoreno5@ggc.edu>
2017-10-17 12:42:44 -0400

    Jose Moreno Test Cases

--- Lipidlator2.1/app/src/androidTest/java/teamtriplej/com/lipidlator21/Glycerophospholipids_ActivityTest.java
+++ Lipidlator2.1/app/src/androidTest/java/teamtriplej/com/lipidlator21/Glycerophospholipids_ActivityTest.java
@@ -1,4 +1,61 @@
 package teamtriplej.com.lipidlator21;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.rule.ActivityTestRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jose on 10/15/2017.
+ */
+
 public class Glycerophospholipids_ActivityTest {
+    @Rule
+    public ActivityTestRule<Glycerophospholipids_Activity> mActivityTestRule = new ActivityTestRule<Glycerophospholipids_Activity>(Glycerophospholipids_Activity.class);
+
+    private Glycerophospholipids_Activity mActivity = null;
+
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(Glycerophospholipids_Result_Activity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor home = getInstrumentation().addMonitor(HomeActivity.class.getName(),null,false);
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+
+    }
+
+    //This tests out the submit button and it should take you to the Result screen to pass
+    @Test
+    public void testLaunchOfResultActivity()
+    {
+        assertNotNull(mActivity.findViewById(R.id.btnSubmit));
+        onView(withId(R.id.btnSubmit)).perform(click());
+        Activity Glycerophospholipids_Result = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        assertNotNull(Glycerophospholipids_Result);
+        Glycerophospholipids_Result.finish();
+    }
+
+    //This tests out the back button and it should take you to the Home screen to pass
+    @Test
+    public void testLaunchOfHomeActivity()
+    {
+        assertNotNull(mActivity.findViewById(R.id.btnBack));
+        onView(withId(R.id.btnBack)).perform(click());
+        Activity homeScreen = getInstrumentation().waitForMonitorWithTimeout(home,5000);
+        assertNotNull(homeScreen);
+        homeScreen.finish();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity=null;
+    }
 }
