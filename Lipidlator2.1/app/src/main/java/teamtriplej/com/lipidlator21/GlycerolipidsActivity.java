19f1c8d038223a0faf80415a92492f8bf6def707
Jessica Sok <jessicasok11@gmail.com>
2017-10-20 19:53:45 -0400

    Glycerolipids sn1 calculation for result screen

--- Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/GlycerolipidsActivity.java
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/GlycerolipidsActivity.java
@@ -5,19 +5,54 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import static teamtriplej.com.lipidlator21.R.id.spnIon;
 
 public class GlycerolipidsActivity extends AppCompatActivity {
+
+    // Variables
+    public int ionIndex, sn1Index, sn2Index, sn3Index;
+    public String ionSelected,sn1Selected, sn2Selected, sn3Selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glycerolipids);
+
+        // Spinners and Buttons
+        final Spinner spnI = (Spinner) findViewById(spnIon);
+        final Spinner spnSn1 = (Spinner) findViewById(R.id.spnSn1);
+        final Spinner spnSn2 = (Spinner) findViewById(R.id.spnSn2);
+        final Spinner spnSn3 = (Spinner) findViewById(R.id.spnSn3);
         Button btnBack = (Button) findViewById(R.id.btnBack);
         Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        // Submit Button
         btnSubmit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                // Get spinners' data
+                ionIndex = spnI.getSelectedItemPosition();
+                sn1Index = spnSn1.getSelectedItemPosition();
+                sn2Index = spnSn2.getSelectedItemPosition();
+                sn3Index = spnSn3.getSelectedItemPosition();
+                ionSelected = spnI.getSelectedItem().toString();
+                sn1Selected = spnSn1.getSelectedItem().toString();
+                sn2Selected = spnSn2.getSelectedItem().toString();
+                sn3Selected = spnSn3.getSelectedItem().toString();
                 Intent intent = new Intent(GlycerolipidsActivity.this, Glycerolipids_Result_Activity.class);
+
+                // putExtra to transfer data to result screen activity
+                intent.putExtra("ionIndex", ionIndex);
+                intent.putExtra("sn1Index", sn1Index);
+                intent.putExtra("sn2Index", sn2Index);
+                intent.putExtra("sn3Index", sn3Index);
+                intent.putExtra("ionSelected", ionSelected);
+                intent.putExtra("sn1Selected", sn1Selected);
+                intent.putExtra("sn2Selected", sn2Selected);
+                intent.putExtra("sn3Selected", sn3Selected);
+
                 startActivity(intent);
             }
         });
--- Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/Glycerolipids_Result_Activity.java
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/Glycerolipids_Result_Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Glycerolipids_Result_Activity extends AppCompatActivity {
 
@@ -14,6 +15,42 @@ public class Glycerolipids_Result_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_glycerolipids_result);
         Button btnBack = (Button) findViewById(R.id.btnBack);
         Button btnHome = (Button) findViewById(R.id.btnHome);
+
+        // TextView
+        TextView ionResult = (TextView) findViewById(R.id.tvIonResult);
+        TextView molarMassResult = (TextView) findViewById(R.id.tvMolarMassResult);
+        TextView formulaResult = (TextView) findViewById(R.id.tvFormulaResult);
+        TextView abvResult = (TextView) findViewById(R.id.tvAbbreviationResult);
+        TextView sn1Result = (TextView) findViewById(R.id.tvSn1Result);
+        TextView sn2Result = (TextView) findViewById(R.id.tvSn2Result);
+        TextView sn3Result = (TextView) findViewById(R.id.tvSn3Result);
+
+        // Get Extras
+        int ionIndex = getIntent().getExtras().getInt("ionIndex");
+        int sn1Index = getIntent().getExtras().getInt("sn1Index");
+        int sn2Index = getIntent().getExtras().getInt("sn2Index");
+        int sn3Index = getIntent().getExtras().getInt("sn3Index");
+        String ionSelected = getIntent().getExtras().getString("ionSelected");
+        String sn1Selected = getIntent().getExtras().getString("sn1Selected");
+        String sn2Selected = getIntent().getExtras().getString("sn2Selected");
+        String sn3Selected = getIntent().getExtras().getString("sn3Selected");
+
+        Calculations calc = new Calculations();
+        double mass = calc.calculateGBasicMass(sn1Index);
+        double molarMass = Math.round(calc.calculateFinalMass(ionIndex, mass) * 10000d) / 10000d;
+        String formula = calc.calculateFormula(calc.getNumC(), calc.getNumH(), calc.getNumO(), calc.getNumN(),
+                calc.getNumAg(), calc.getNumLi(), calc.getNumNa(), calc.getNumK(), calc.getNumCl(),
+                 calc.getNumP(), calc.getNumS(), calc.getNumF());
+
+        // Set Text
+        ionResult.setText(ionSelected);
+        sn1Result.setText(sn1Selected);
+        sn2Result.setText(sn2Selected);
+        sn3Result.setText(sn3Selected);
+        abvResult.setText("MG(" + sn1Selected + "/" + sn2Selected + "/" + sn3Selected + ")");
+       // formulaResult.setText(formula);
+        molarMassResult.setText(Double.toString(molarMass));
+
         btnBack.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
