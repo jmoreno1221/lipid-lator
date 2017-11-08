276611d5c875cc88cde654d96faee2a8e1809bad
Joseph Tsegaye <josephtsegaye@gmail.com>
2017-11-07 20:07:00 -0500

    Wax Ester Integration Testing Set Up

--- Lipidlator2.1/app/src/androidTest/java/teamtriplej/com/lipidlator21/WaxEstersActivityTest.java
+++ Lipidlator2.1/app/src/androidTest/java/teamtriplej/com/lipidlator21/WaxEstersActivityTest.java
@@ -5,15 +5,22 @@ import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
+import static teamtriplej.com.lipidlator21.R.id.imbtnCholesterylEsters;
 
 /* @Author: Jessica Sok
  * @Date: 10/16/2017
@@ -32,24 +39,66 @@ public class WaxEstersActivityTest {
         mActivity = mActivityTestRule.getActivity();
     }
 
-    // Test submit button to result screen
+    //Integration Test between Selection Screen and Calculation Screen
     @Test
-    public void testLaunchOfResultActivity(){
-        assertNotNull(mActivity.findViewById(R.id.btnSubmit));
+    public void selectionIonTest()
+    {
+        onView(withId(R.id.spnIon)).perform(click());
+        onData(anything()).atPosition(5).perform(click());
+        onView(withId(R.id.spnIon)).check(matches(withSpinnerText(containsString("[M+K]+"))));
+
+        Assert.assertNotNull(mActivity.findViewById(R.id.btnSubmit));
         onView(withId(R.id.btnSubmit)).perform(click());
-        Activity waxEster_Result = getInstrumentation().waitForMonitorWithTimeout(monitor, 5000);
-        assertNotNull(waxEster_Result);
-        waxEster_Result.finish();
+        Activity WaxEster_Result = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        Assert.assertNotNull(WaxEster_Result);
     }
 
-    // Test back button to go to home screen
+    //Integration Test between Selection Screen and Calculation Screen
     @Test
-    public void testLaunchOfHomeActivity(){
-        assertNotNull(mActivity.findViewById(R.id.btnBack));
+    public void selectionAlcoholTest()
+    {
+        onView(withId(R.id.spnAlcohol)).perform(click());
+        onData(anything()).atPosition(4).perform(click());
+        onView(withId(R.id.spnAlcohol)).check(matches(withSpinnerText(containsString("10:0"))));
+
+        Assert.assertNotNull(mActivity.findViewById(R.id.btnSubmit));
+        onView(withId(R.id.btnSubmit)).perform(click());
+        Activity WaxEster_Result = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        Assert.assertNotNull(WaxEster_Result);
+    }
+
+    //Integration Test between Selection Screen and Calculation Screen
+    @Test
+    public void selectionAcidTest()
+    {
+        onView(withId(R.id.spnAcid)).perform(click());
+        onData(anything()).atPosition(5).perform(click());
+        onView(withId(R.id.spnAcid)).check(matches(withSpinnerText(containsString("12:0"))));
+
+        Assert.assertNotNull(mActivity.findViewById(R.id.btnSubmit));
+        onView(withId(R.id.btnSubmit)).perform(click());
+        Activity WaxEster_Result = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        Assert.assertNotNull(WaxEster_Result);
+    }
+
+    //This tests out the submit button and it should take you to the Result screen to pass
+    @Test
+    public void testLaunchOfResultActivity()
+    {
+        Assert.assertNotNull(mActivity.findViewById(R.id.btnSubmit));
+        onView(withId(R.id.btnSubmit)).perform(click());
+        Activity WaxEster_Result = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        Assert.assertNotNull(WaxEster_Result);
+        WaxEster_Result.finish();
+    }
+
+    //This tests out the back button and it should take you to the Home screen to pass
+    @Test
+    public void testLaunchOfHomeActivity()
+    {
+        Assert.assertNotNull(mActivity.findViewById(R.id.btnBack));
         onView(withId(R.id.btnBack)).perform(click());
-        Activity homeScreen = getInstrumentation().waitForMonitorWithTimeout(home, 5000);
-        assertNotNull(homeScreen);
-        homeScreen.finish();
+        mActivity.findViewById(imbtnCholesterylEsters);
     }
 
     @After
