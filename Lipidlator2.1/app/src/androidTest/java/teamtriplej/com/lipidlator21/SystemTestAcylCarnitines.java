f533480105a66752d347b48eba9e8bdc7e25e647
Joseph Tsegaye <josephtsegaye@gmail.com>
2017-11-11 15:24:56 -0500

    Understanding System Testing for Acyl Carnitines

---
+++ Lipidlator2.1/app/src/androidTest/java/teamtriplej/com/lipidlator21/SystemTestAcylCarnitines.java
@@ -0,0 +1,60 @@
+package teamtriplej.com.lipidlator21;
+
+/* @Author: Jessica Sok
+ * @Date: 11/10/2017
+ */
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertNotNull;
+
+public class SystemTestAcylCarnitines {
+    @Rule
+    public ActivityTestRule<HomeActivity> homeActivityActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
+    private Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(AcylCarnitines_Result_Activity.class.getName(),null,false);
+
+    private HomeActivity mhomeActivity = null;
+
+
+    //Declaring and initialize the variable
+    @Before
+    public void setUp() throws Exception {
+        mhomeActivity = homeActivityActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void fullTest() {
+        assertNotNull(mhomeActivity.findViewById(R.id.imbtnAcylCarnitines));
+        onView(withId(R.id.imbtnAcylCarnitines)).perform(click());
+        mhomeActivity.findViewById(R.id.spnIon);
+        mhomeActivity.findViewById(R.id.spnAcylChain);
+        mhomeActivity.findViewById(R.id.btnSubmit);
+        onView(withId(R.id.btnSubmit)).perform(click());
+        Activity AcylCarnitines_Result = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        assertNotNull(AcylCarnitines_Result);
+        mhomeActivity.findViewById(R.id.btnBack);
+        onView(withId(R.id.btnBack)).perform(click());
+        mhomeActivity.findViewById(R.id.btnSubmit);
+        onView(withId(R.id.btnSubmit)).perform(click());
+        assertNotNull(AcylCarnitines_Result);
+        mhomeActivity.findViewById(R.id.btnHome);
+        onView(withId(R.id.btnHome)).perform(click());
+        assertNotNull(mhomeActivity);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mhomeActivity = null;
+    }
+}
