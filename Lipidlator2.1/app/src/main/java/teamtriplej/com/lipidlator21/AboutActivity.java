fc6e5a8b21603d68d1270667862c71da426c5968
Jessica Sok <jessicasok11@gmail.com>
2017-11-18 02:53:37 -0500

    Finished About Lipid-Lator Screen. Included in progress help menu bar

--- Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/AboutActivity.java
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/AboutActivity.java
@@ -1,18 +1,21 @@
 package teamtriplej.com.lipidlator21;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
-    public String title = "Lipid-Lator";
+    public String title = "About Lipid-Lator";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,7 @@ public class AboutActivity extends AppCompatActivity {
         if (bar != null) {
             bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
         }
+
         View view = getLayoutInflater().inflate(R.layout.action_bar, null);
         ActionBar.LayoutParams params = new ActionBar.LayoutParams(
                 ActionBar.LayoutParams.WRAP_CONTENT,
@@ -30,17 +34,43 @@ public class AboutActivity extends AppCompatActivity {
         TextView Title = (TextView) view.findViewById(R.id.actionbar_title);
         Title.setText(title);
         Title.setTextColor(Color.parseColor("#FFFFFF"));
+        Title.setTextSize(36);
 
         getSupportActionBar().setCustomView(view,params);
         getSupportActionBar().setDisplayShowCustomEnabled(true); //show custom title
         getSupportActionBar().setDisplayShowTitleEnabled(false); //hide the default title
 
-        Button ok = (Button) findViewById(R.id.btnOK);
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
+
+
+        Button btnGotIt = (Button) findViewById(R.id.button);
+
+        btnGotIt.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View view){
+               finish();
+           }
         });
+
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_help){
+            finish();
+            Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+            // do something here
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
