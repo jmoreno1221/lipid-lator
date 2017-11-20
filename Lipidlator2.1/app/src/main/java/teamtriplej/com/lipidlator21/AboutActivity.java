76087a68787387a7119cf2895b5409d1fa327fa1
jmoreno1221 <jmoreno5@ggc.edu>
2017-11-20 00:34:33 -0500

    help menu update

--- Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/AboutActivity.java
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/AboutActivity.java
@@ -1,14 +1,11 @@
 package teamtriplej.com.lipidlator21;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -53,24 +50,4 @@ public class AboutActivity extends AppCompatActivity {
 
     }
 
-    // create an action bar button
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    // handle button activities
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.menu_help){
-            finish();
-            Intent intent = new Intent(this, HelpActivity.class);
-            startActivity(intent);
-            // do something here
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
--- Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/HelpActivity.java
+++ Lipidlator2.1/app/src/main/java/teamtriplej/com/lipidlator21/HelpActivity.java
@@ -1,13 +1,39 @@
 package teamtriplej.com.lipidlator21;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
+    private String title = "How To Use Lipid-Lator";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+        }
+
+        View view = getLayoutInflater().inflate(R.layout.action_bar, null);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                Gravity.CENTER);
+
+        TextView Title = (TextView) view.findViewById(R.id.actionbar_title);
+        Title.setText(title);
+        Title.setTextColor(Color.parseColor("#FFFFFF"));
+        Title.setTextSize(36);
+
+        getSupportActionBar().setCustomView(view,params);
+        getSupportActionBar().setDisplayShowCustomEnabled(true); //show custom title
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //hide the default title
     }
 }
